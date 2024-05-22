3b928365392eabc477bdf5951368d1cc304beaa6
Luís <luisfca@outlook.pt>
2024-05-22 19:42:15 +0100

    Criação de todos os componentes. Feito reset ao CSS. Atualização do favicon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import Adotar from './components/Adotar';
+import Contactos from './components/Contactos';
+import Doar from './components/Doar';
+import SobreNos from './components/SobreNos';
+import Voluntariado from './components/Voluntariado';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <NavBar/>
+      <SobreNos/>
+      <Adotar/>
+      <Voluntariado/>
+      <Doar/>
+      <Contactos/>
+      <Footer/>
     </div>
   );
 }
