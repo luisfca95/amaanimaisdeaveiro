4d271aa369d985db956b087101a9ccb4a24b0c67
Luís <luisfca@outlook.pt>
2024-05-23 20:08:31 +0100

    Alterado tipo de letra do NavBar. Criação do conteúdo do Footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,23 @@
+import '../styles/Footer.css';
+
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <footer className="footer">
+            <div className="footer-address">
+                <address>
+                    Rua da Infância <br/>
+                    Quinta do Loureiro
+                </address>
+
+            </div>
+            <div className="footer-social">
+                <a href="https://www.facebook.com/AbrigoAMAveiro">
+                    Facebook
+                </a>
+                <a href="https://www.instagram.com/ama.animaisdeaveiro/">
+                    Instagram
+                </a>
+            </div>
+        </footer>
     );
 }
